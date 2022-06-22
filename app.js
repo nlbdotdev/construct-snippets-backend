4741c76cf5af7491d56e5a7d22caa49df32366a5
ReflextionsDev <reflextionsdev@gmail.com>
2022-06-22 13:20:04 -0400

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
 var createError = require('http-errors');
+var cors = require('cors')
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -24,6 +25,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
