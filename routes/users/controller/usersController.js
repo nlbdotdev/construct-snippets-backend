052bfcc3b3c6358d0f80f6dc01f54cd6a249f304
ReflextionsDev <reflextionsdev@gmail.com>
2022-07-05 21:05:30 -0400

    change jwt token to res cookie

--- routes/users/controller/usersController.js
+++ routes/users/controller/usersController.js
@@ -4,9 +4,33 @@ const User = require("../model/User")
 const { userErrorHandler: errorHandler } = require('../utils/userErrorHandler')
 const { hashPassword, getUserFromToken } = require("../utils/userFunctions")
 
+
+// Remove sensitive data from user
+const cleanUser = (user) => {
+    return {
+        id: user._id,
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+    }
+}
+
+// Generate jwt token for user  sessions
+const getToken = (userId) => {
+    const token = jwt.sign(
+        { userId },
+        process.env.SECRET_KEY,
+        { expiresIn: process.env.JWT_LIFETIME }
+    )
+
+    return token
+}
+
 // Create new user
 const createUser = async (req, res) => {
     try {
+
         // Create user with body params and hashed password
         const { firstName, lastName, username, email, password } = req.body
         let hashedPassword = await hashPassword(password)
@@ -19,7 +43,10 @@ const createUser = async (req, res) => {
         })
         let savedUser = await newUser.save()
 
-        res.status(200).json({ message: "success", payload: savedUser })
+        // Generate a token on sucesful login
+        const jwtToken = getToken(savedUser._id)
+        res.cookie('session_token', jwtToken)
+        res.status(200).json({ message: "success", payload: cleanUser(savedUser) })
 
     } catch (error) {
         console.log(error)
@@ -27,9 +54,41 @@ const createUser = async (req, res) => {
     }
 }
 
+// Login with email and password
+const userLogin = async (req, res) => {
+    try {
+
+        // Get user by email
+        const { email, password } = req.body
+        const foundUser = await User.findOne({ email: email })
+        if (foundUser === null) throw { message: "No user found for this email/password", type: "nomatch" }
+
+        // Validate password
+        const comparedPassword = await bcrypt.compare(password, foundUser.password)
+        if (!comparedPassword) throw { message: "No user found for this email/password", type: "nomatch" }
+
+        // Generate a token on sucesful login
+        const jwtToken = getToken(foundUser._id)
+        res.cookie('session_token', jwtToken)
+        res.status(200).json({ message: "success", payload: cleanUser(foundUser) })
+
+    } catch (error) {
+        res.status(500).json({ error: error.message, type: error.type })
+    }
+}
+
+// Get current user from bearer token
+const getCurrentUser = async (req, res) => {
+    try {
+        const foundUser = await getUserFromToken(res.locals.decodedToken)
+        res.status(200).json({ message: "Current user", payload: foundUser });
+    } catch (error) {
+        res.status(500).json({ error: errorHandler(error) });
+    }
+}
+
 // Update current user with bearer token and req body
 const updateProfile = async (req, res) => {
-
     try {
         // Fetch token from response locals
         const decodedToken = res.locals.decodedToken
@@ -47,50 +106,6 @@ const updateProfile = async (req, res) => {
 
     } catch (error) {
         res.status(500).json({ error: errorHandler(error) })
-    }
-}
-
-// Login with email and password
-const userLogin = async (req, res) => {
-
-    try {
-
-        // Validate email
-        const { email, password } = req.body
-        const foundUser = await User.findOne({ email: email })
-        // More secure to use same error to obscure email
-        // if (foundUser === null) throw { message: "Email not found" }
-
-        if (foundUser === null) throw { message: "No user found for this email/password", type: "nomatch" }
-
-        // Validate password
-        const comparedPassword = await bcrypt.compare(password, foundUser.password)
-        if (!comparedPassword) throw { message: "No user found for this email/password", type: "nomatch" }
-
-        // Generate a token on sucesful login
-        const jwtToken = jwt.sign(
-            {
-                firstName: foundUser.firstName,
-                lastName: foundUser.lastName,
-                email: foundUser.email,
-                username: foundUser.username,
-            },
-            process.env.SECRET_KEY,
-            { expiresIn: process.env.JWT_LIFETIME }
-        )
-        res.status(200).json({ payload: jwtToken })
-    } catch (error) {
-        res.status(500).json({ error: error.message, type: error.type })
-    }
-}
-
-// Get current user from bearer token
-const getCurrentUser = async (req, res) => {
-    try {
-        const foundUser = await getUserFromToken(res.locals.decodedToken)
-        res.status(200).json({ message: "Current user", payload: foundUser });
-    } catch (error) {
-        res.status(500).json({ error: errorHandler(error) });
     }
 }
 
